8c92fe0e1286b374108bb44d47f4b78d246c4ddc
Manomoy Biswas <manomoy.biswas@rently.com>
2025-02-06 20:35:24 +0530

    added expected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :experiences do
+        resources :bookings
+        resources :reviews
+      end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+      resources :users do
+        resources :messages
+      end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+      resources :payments
+    end
+  end
 end
