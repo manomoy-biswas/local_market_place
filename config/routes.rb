ab637fbc0aa5c46a6df411111eb4e0973056d665
Manomoy Biswas <manomoy.biswas@rently.com>
2025-02-07 02:33:49 +0530

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,36 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :experiences do
-        resources :bookings
+        member do
+          get :check_availability
+        end
+        collection do
+          get :host_experiences
+        end
+        resources :bookings do
+          member do
+            post :cancel
+          end
+        end
         resources :reviews
       end
 
       resources :users do
-        resources :messages
+        resources :messages do
+          member do
+            post :mark_as_read
+          end
+          collection do
+            get "conversation/:user_id", to: "messages#conversation"
+          end
+        end
       end
 
-      resources :payments
+      resources :payments do
+        member do
+          post :refund
+        end
+      end
     end
   end
 end
